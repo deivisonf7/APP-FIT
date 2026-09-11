b086ad4e2b6d4e01c8a7f17472802fc94d99ab57
deivisonf7 <deivisonf7@gmail.com>
2026-09-11 14:45:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
-const CACHE_NAME = 'v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icone.png'];
+const CACHE_NAME = 'treinos-v2'; // Mudamos a versão para forçar a atualização
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icone.png'
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Força o novo Service Worker a ativar na hora
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Deleta o cache com bug anterior
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
